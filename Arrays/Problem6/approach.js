c93d12ea1b17754886bedbc98d67510ae27655c2
Tayyab Ahmed <tayyabahmed7561@gmail.com>
2026-07-21 21:20:49 +0500

    solve problem find second largest number

---
+++ Arrays/Problem6/approach.js
@@ -0,0 +1,22 @@
+// find second largest number
+
+let arr = [9, 4, 1, 7, 11, 78, 45];
+
+function FindSecondLargest(arr) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] < largest) {
+      secondLargest = arr[i];
+    }
+  }
+
+  return [largest, secondLargest];
+}
+
+let check = FindSecondLargest(arr);
+console.log(check);
